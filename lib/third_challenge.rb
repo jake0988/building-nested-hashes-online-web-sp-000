2556105f794775595b00a356815a7b0c60e56d54
Jake G <jake098877@gmail.com>
2020-04-30 15:51:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,18 +1,13 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        { name: "Lord Montague", age: "53" }
-        #your key/value pairs here
-        },
-      :matriarch => {
-        { name: "Lady Montague", age: "54" }
-        #your key/value pairs here
-        },
-      :hero => {
-        { name: "Romeo", age: "15", status: "alive" }
+      :patriarch =>
+        { name: "Lord Montague", age: "53" },
+      :matriarch =>
+        { name: "Lady Montague", age: "54" },
+      :hero => { name: "Romeo", age: "15", status: "alive" }
       :hero_friends => [ { name: "Benvolio", age: "17", attitude: "worried"  }, {  name: "Mercutio", age: "18", attitude: "hot-headed"  } ]
-   }
+
    :capulet => {
       :patriarch =>
         { name: "Lord Capulet", age: 51 },
